6ee8ade27e07c1cd66d360cd210d4042bf545dcf
sebastien leridon <sebleridon@laposte.net>
2021-12-09 19:43:29 +0100

    Fix HSQL compliance DB creation scripts for tests

--- src/sql/plugins/mydashboard/plugin/create_db_mydashboard.sql
+++ src/sql/plugins/mydashboard/plugin/create_db_mydashboard.sql
@@ -13,9 +13,9 @@ CREATE TABLE mydashboard_configuration (
 
 DROP TABLE IF EXISTS mydashboard_panel;
 CREATE TABLE mydashboard_panel (
-id_panel int(6) NOT NULL,
-code varchar(50) NOT NULL default '',
-title varchar(255) NOT NULL default '',
+id_panel int NOT NULL,
+code varchar(50)  default '' NOT NULL,
+title varchar(255)  default '' NOT NULL,
 description long varchar NULL,
 is_default  SMALLINT NOT NULL,
 PRIMARY KEY (id_panel)
@@ -29,10 +29,10 @@ PRIMARY KEY (id_panel)
 
 DROP TABLE IF EXISTS mydashboard_dashboard_association;
 CREATE TABLE mydashboard_dashboard_association (
-id_dashboard_association  int(6) NOT NULL,
-id_dashboard varchar(50) NOT NULL default '0',
-id_panel int(11) NOT NULL default '0',
-position int(11) NOT NULL default '0',
+id_dashboard_association  int NOT NULL,
+id_dashboard varchar(50) default '0' NOT NULL ,
+id_panel int default '0' NOT NULL ,
+position int default '0' NOT NULL ,
 PRIMARY KEY (id_dashboard_association)
 );
 
@@ -49,7 +49,7 @@ ALTER TABLE mydashboard_dashboard_association ADD CONSTRAINT fk_id_panel FOREIGN
 DROP TABLE IF EXISTS mydashboard_portlet_panel;
 CREATE TABLE mydashboard_portlet_panel (
 id_portlet int NOT NULL,
-id_panel int(11) NOT NULL default '0',
+id_panel int(11) default '0' NOT NULL ,
 PRIMARY KEY ( id_portlet )
 );
 
