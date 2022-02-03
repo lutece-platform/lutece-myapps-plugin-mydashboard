c2ef94439604d5f036760c427fc0234c78d9a231
rdeniel <renaud.deniel@outlook.com>
2022-02-03 18:24:42 +0100

    MYDASHBOARD-17 added a unit test

---
+++ src/test/java/fr/paris/lutece/plugins/mydashboard/business/DashboardAssociationTest.java
@@ -0,0 +1,14 @@
+package fr.paris.lutece.plugins.mydashboard.business;
+
+import org.junit.Test;
+
+import fr.paris.lutece.test.LuteceTestCase;
+
+public class DashboardAssociationTest extends LuteceTestCase
+{
+    @Test
+    public void testDashboardAssociation( )
+    {
+        assertTrue( true );
+    }
+}
